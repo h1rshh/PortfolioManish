3da8f3102fb0063bad35ebd6730d71518184e157
h1rshh <harshvardhanshekhawat815gmail.com>
2024-11-04 21:08:59 +0530

    okay

--- app/components/Footers.js
+++ app/components/Footers.js
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react';
-import { FaInstagram, FaFacebookF, FaLinkedinIn, FaYoutube } from 'react-icons/fa';
+import { FaInstagram, FaFacebookF, FaLinkedinIn, FaXTwitter } from 'react-icons/fa6'; // Import FaXTwitter from react-icons/fa6
 
 const Footers = () => {
   return (
@@ -14,17 +14,18 @@ const Footers = () => {
         <p className="mb-6">Digir Innovation:  +917014834063</p>
 
         <div className="flex justify-center space-x-6 text-2xl mb-4">
-          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
+          <a href="https://www.instagram.com/manishvyas_05/" target="_blank" rel="noopener noreferrer" className="hover:text-pink-500">
             <FaInstagram />
           </a>
-          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
+          <a href="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C589460569849%7Cb%7Cfacebook%7C&placement=&creative=589460569849&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-592856129%26loc_physical_ms%3D9179342%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gad_source=1&gclid=Cj0KCQiA_qG5BhDTARIsAA0UHSKuR3nIrFCo5yfXS4b5F5oueTRKAlFnjtHDzSVeHF6daQ2N3oYx89YaAg6BEALw_wcB" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600">
             <FaFacebookF />
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+          <a href="https://www.linkedin.com/company/digir-innovations-and-technologies/posts/?feedView=all" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
             <FaLinkedinIn />
           </a>
-          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer" className="hover:text-red-600">
-            <FaYoutube />
+          {/* Use FaXTwitter from react-icons/fa6 */}
+          <a href="https://x.com/DigiR_Raj" target="_blank" rel="noopener noreferrer" className="hover:text-[#4a4c59]">
+            <FaXTwitter />
           </a>
         </div>
 
